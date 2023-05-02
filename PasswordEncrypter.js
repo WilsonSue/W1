62babd894285f32e2e892dc2e3ab3767ea5c9c2c
WilsonSue <113160128+WilsonSue@users.noreply.github.com>
2023-05-01 22:27:08 -0700

    Add code

--- PasswordEncrypter.js
+++
@@ -1,6 +0,0 @@
-// const bcrypt = require("bcrypt");
-// const plainPassword = "pass1";
-
-// const encryptedPassword = bcrypt.hashSync(plainPassword, 10);
-// console.log(encryptedPassword);
-// console.log(bcrypt.compareSync("pass1", encryptedPassword)); // true
